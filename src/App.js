5a3c86e5a5d74a1a22acfab70446c13f464158b2
Sasha Surovtsev <neon4ik378@gmail.com>
2024-10-16 01:48:19 +0300

    adding adaptability to the site and profile window

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-
 import Header from "./Header";
 import Profile from "./Profile";
-export default function App() {
 
+export default function App() {
   return (
-      <div className="Window flex flex-col h-screen w-screen">
-        <Header/>
-        <div className="MainContent flex gap-10 h-full w-full bg-red-200">
-            <div className="LeftBar h-full w-52 bg-green-200 border" >
-            </div>
-            <div className="RightBar h-full w-52 bg-green-200 border">
-            <Profile/>
-            </div>
+    <div className="Window flex flex-col h-screen w-full">
+      <div className="max-w-screen-2xl w-full mx-auto flex flex-col h-full">
+        <div className="w-full">
+          <Header />
+        </div>
+        <div className="MainContent py-10 px-4 sm:px-8 md:px-16 flex flex-grow gap-4 sm:gap-8 md:gap-16 w-full bg-red-200 h-full">
+          {/* Левый сайдбар */}
+          <div className="LeftBar w-20 sm:w-48 bg-green-200 border h-full overflow-y-auto">
+            {/* Левый контент */}
+          </div>
+          {/* Правый сайдбар с компонентом Profile */}
+          <div className="RightBar flex-grow bg-green-200 border h-full overflow-y-auto">
+            <Profile />
+          </div>
         </div>
       </div>
-
+    </div>
   );
 }
-
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,7 +1,29 @@
-export default function Profile(){
-  return(
-      <div className="Profile">
+import './Profile.css';
 
-      </div>
-  )
+export default function Profile() {
+  return (
+    <div className="Profile">
+      <header className="Header flex border-2 justify-center items-center gap-10 h-20">
+        <span>All Users</span>
+        <span>C++</span>
+        <span>C#</span>
+        <span>Full-Stack</span>
+        <span>Frontend</span>
+        <span>Backend</span>
+      </header>
+      <main className="Main">
+        {/* Сетка элементов */}
+        <div className="square square1"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+      </main>
+    </div>
+  );
 }
