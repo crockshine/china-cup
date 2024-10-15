15e5a3de88a0811b59e7bde434e36e1c7b634bf8
crockshine <mihail_korolev1111@mail.ru>
2024-10-15 22:26:38 +0300

    begin style

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@
 export default function App() {
 
   return (
-    <div className="App w-screen h-screen">
-        <div className=""></div>
-    </div>
+      <div className="Header border-2 w-screen h-32 flex">
+        <div className="flex-1 border-2 flex justify-center items-center">
+            <span className="font-semibold text-3xl text-slate-700">dsadsa</span>
+        </div>
+        <div className="flex-1 border-2"></div>
+        <div className="flex-1 border-2"></div>
+        <div className="flex-1 border-2"></div>
+      </div>
   );
 }
 
