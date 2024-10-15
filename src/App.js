a98a04022a9a5a95f54cb2ca39e07620e7b9ff2e
Sasha Surovtsev <neon4ik378@gmail.com>
2024-10-15 23:20:02 +0300

    adding a hierarchy and defining page sizes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import Header from "./Header";
+import Profile from "./Profile";
 export default function App() {
 
   return (
-      <div className="Window h-screen w-screen">
+      <div className="Window flex flex-col h-screen w-screen">
         <Header/>
-
+        <div className="MainContent flex gap-10 h-full w-full bg-red-200">
+            <div className="LeftBar h-full w-52 bg-green-200 border" >
+            </div>
+            <div className="RightBar h-full w-52 bg-green-200 border">
+            <Profile/>
+            </div>
+        </div>
       </div>
 
   );
