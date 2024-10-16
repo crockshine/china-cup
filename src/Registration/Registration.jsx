7472e382debcdbfdf9b59af26602719c3e97a608
Mihail_korolev_desctop <mihail_korolev1111@mail.ru>
2024-10-16 16:22:03 +0300

    router

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -1,13 +1,20 @@
 import './Registration.css';
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Registration(){
+    const navigate = useNavigate(); // Хук для навигации
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        navigate('/home');
+    };
   return(
     <main className="main">
       <div className="center_block">
         <h1 className="reg_label">Registration Form</h1>
         <h2 className="about_label">Please fill out this form with the required information</h2>
         <div className="steps_verification">
-        <form className="form" id="loginForm" action="login" method="post">
+        <form className="form" id="loginForm" action="login" method="post" onSubmit={handleSubmit}>
               <div className="mail_input">
               <p className='label'>Email</p>
               <input className="input" name="usermail" id="usermail"/>
@@ -16,7 +23,7 @@ export default function Registration(){
                 <p className='label'>Password</p>
                 <input className="input" name="userpassword" id="userpassword"/>
                 </div>
-                <input className="submit_button" type="submit" value="Join" />
+                <Link to={'/home'}><input className="submit_button" type="submit" value="Join" /></Link>
             </form>
         </div>
       </div>
---
+++ src/SuccessAuth.jsx
@@ -0,0 +1,45 @@
+import Header from "./Header/Header";
+import LeftBar from "./LeftBar/LeftBar";
+import { Link, Route, Routes} from "react-router-dom";
+import Profile from "./Profile/Profile";
+
+export default function SuccessAuth(){
+    return(
+<>
+            <Header />
+
+            <div className="MainContent py-10 px-4 sm:px-8 md:px-16 flex flex-grow gap-4 sm:gap-8 md:gap-16 w-full h-full overflow-hidden">
+
+                <div className="LeftBar h-full px-4 sm:px-10 pb-10 w-80 border flex flex-col items-center justify-between overflow-auto">
+                    <LeftBar/>
+                </div>
+
+                <div className="RightBar flex-grow shadow-2xl rounded-2xl bg-slate-50  border h-full overflow-y-auto">
+                    <Routes>
+                        <Route path='/graduates' element={<Profile/>}></Route>
+                        <Route path='/messenger' element={<Profile/>}></Route>
+                        <Route path='/dashboard' element={<Profile/>}></Route>
+                        <Route path='/tasks' element={<Profile/>}></Route>
+                        <Route path='/schedule' element={<Profile/>}></Route>
+                        <Route path='/files' element={<Profile/>}></Route>
+                        <Route path='/' element={<div
+                            className="w-full h-full flex flex-col items-center justify-center font-bold text-slate-600 text-4xl">
+                            <p>Hello user!</p>
+                        </div>}></Route>
+                        <Route path='/*' element={
+                            <div
+                                className="w-full h-full flex flex-col items-center justify-center font-bold text-slate-600 text-2xl">
+                                <p>No page is selected.</p>
+                                <Link to={'/home'}>
+                                    <p className="underline">To home</p>
+                                </Link>
+
+                            </div>}/>
+
+                    </Routes>
+                </div>
+
+            </div>
+</>
+    )
+}
