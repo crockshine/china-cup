55321ad19e340f700c761e48b80cfe10e89286f6
Sasha Surovtsev <neon4ik378@gmail.com>
2024-10-16 16:24:05 +0300

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import Header from "./Header/Header";
 import Profile from "./Profile/Profile";
 import LeftBar from "./LeftBar/LeftBar";
+import Tasks from "./Tasks"
+import './App.css';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 export default function App() {
     return (
       <div className="Window bg-slate-100 flex flex-col h-screen w-screen overflow-hidden" style={{ fontFamily: 'Roboto,sans-serif' }}>
           <Header />
-
           <BrowserRouter>
-
-
-
             <div className="MainContent py-10 px-4 sm:px-8 md:px-16 flex flex-grow gap-4 sm:gap-8 md:gap-16 w-full h-full overflow-hidden">
-                <LeftBar/>
-
-
+                <LeftBar className="LeftBar"/>
               <div className="RightBar flex-grow shadow-2xl rounded-2xl bg-slate-50  border h-full overflow-y-auto">
                   <Routes>
                           <Route path="*" element={
@@ -27,6 +23,7 @@ export default function App() {
 
                               </div>}/>
                           <Route path="/graduates" element={<Profile />} />
+                          <Route path="/tasks" element={<Tasks/>}/>
 
                   </Routes>
 
---
+++ src/Tasks.jsx
@@ -0,0 +1,35 @@
+import './Tasks.css'
+
+export default function Tasks(){
+  return(
+      <main>
+        <div className="main_blocks">
+          <div className="task_card">
+            <div className="card__title">
+              <p>fweojjwfe</p>
+            </div>
+            <div className="card__description">
+              <div className="deadline">
+                <img className="image_Card" src="./icons/time.jpg" alt="" />
+                  <h1 className="card__label ">Deadline: 09/25/2025</h1>
+              </div>
+              <div className="files__count">
+              <img className="image_Card" src="./icons/download.jpg" alt="" />
+                <h1 className="card__label">4 Files</h1>
+              </div>
+              <div className="tasks__count">
+              <img className="image_Card" src="./icons/count.jpg" alt="" />
+                <h1 className="card__label">Sub Tasks 6</h1>
+              </div>
+              <div className="members__count">
+              <img className="image_Card" src="./icons/members.jpg" alt="" />
+                <h1 className="card__label">Members 5</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+  )
+}
+
