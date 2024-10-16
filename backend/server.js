2ac33a726c52d3b0ba8af5f8bf500497b8c1a366
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2024-10-16 21:12:36 +0300

    create backend))

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Указываем папку со статическими файлами (React build)
+app.use(express.static(path.join(__dirname, '../build')));
+
+require('./routes')(app);
+
+// Обработка всех остальных маршрутов, которые не относятся к API
+/*app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});*/
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
