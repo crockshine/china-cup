584c784407c76810f287ef3d258841f364c45947
Sasha Surovtsev <neon4ik378@gmail.com>
2024-10-16 13:39:35 +0300

    add Registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Profile from "./Profile";
 import LeftBar from "./LeftBar";
+import Registration from "./Registration";
 
 export default function App() {
     return (
-      <div className="Window bg-slate-100 flex flex-col h-screen w-screen overflow-hidden" style={{ fontFamily: 'Roboto,sans-serif' }}>
-        <Header />
+      <div className="Window bg-slate-100 flex flex-col h-screen w-screen overflow-hidden">
+        <Registration/>
+         <Header />
         <div className="MainContent py-10 px-4 sm:px-8 md:px-16 flex flex-grow gap-4 sm:gap-8 md:gap-16 w-full h-full overflow-hidden">
             <LeftBar/>
           <div className="RightBar flex-grow shadow-2xl rounded-2xl bg-slate-50  border h-full overflow-y-auto">
