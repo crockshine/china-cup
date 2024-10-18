0d01fbc49d42ec2899e86611132ff6f7237d1993
Mihail_korolev_desctop <mihail_korolev1111@mail.ru>
2024-10-18 21:24:54 +0300

    l bar

--- src/LeftBar/CardInLeftBar.jsx
+++ src/LeftBar/CardInLeftBar.jsx
@@ -6,43 +6,18 @@ import {useEffect, useState} from "react";
 export default function CardInLeftBar({text,image,opacity, router_link}){
 
     const location = useLocation()
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight); // Инициализация с текущей высотой окна
-    const [isHidden, setIsHidden] = useState(false); // Инициализация с текущей высотой окна
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowHeight(window.innerHeight); // Обновление высоты окна
-        };
-
-        window.addEventListener('resize', handleResize); // Добавление обработчика события
-
-        // Вызов функции при монтировании, чтобы установить начальное значение
-        handleResize();
-
-        return () => {
-            window.removeEventListener('resize', handleResize); // Удаление обработчика при размонтировании
-        };
-    }, []);
-
-    useEffect(() => {
-        if (windowHeight <= 790 ){
-                setIsHidden(true)
-        }else {
-            setIsHidden(false)
-
-        }
-    }, [windowHeight]);
 
     return (
-        <Link  className={`CardInLeftBar ${isHidden && router_link !== location.pathname ? "" : "order-6"}`} to={router_link}
+        <Link  className={`CardInLeftBar ${router_link !== location.pathname ? "" : "order-6 flex-shrink-0"}`} to={router_link}
               onClick={ModalWindow.closeWindow}>
 
-            <div  className={`${isHidden && router_link !== location.pathname ? "p-0 " : "p-4 " } border-2  rounded-2xl text-base font-bold text-slate-600 flex flex-col items-center justify-center h-full  transition 
+            <div  className={`${ !location.pathname.includes(router_link) ? "p-0 " : "p-6 w-full" } border-2  rounded-2xl text-base font-bold text-slate-600 flex flex-col items-center justify-center h-full  transition 
                             
             ${location.pathname.includes(router_link) ? 'bg-slate-50 -translate-y-1 shadow-2xl' : 'bg-gray-100'}`}
                   >
                 <img src={`/icons/${image}`} alt="" style={{opacity: opacity}}/>
-                <span className={`${isHidden  && router_link !== location.pathname ? " hidden" : "block"}`}>{text}</span>
+                <span className={`${ !location.pathname.includes(router_link) ? " hidden" : "block"}`}>{text}</span>
             </div>
 
         </Link>
--- src/ModalWindows/SecondModalWindowWrapper.jsx
+++ src/ModalWindows/SecondModalWindowWrapper.jsx
@@ -1,12 +1,18 @@
 import {observer} from "mobx-react-lite";
 import {Link} from "react-router-dom";
 import React from "react";
+import SecondModalWindow from "../Stores/SecondModalWindow";
+import ModalWindow from "../Stores/ModalWindow";
 const SecondModalWindowWrapper = observer(({ isOpenSecond}) => {
+    const handlerClose = () => {
+        SecondModalWindow.switchWindow()
+        ModalWindow.closeWindow()
+    }
     return (
         <div className={`${isOpenSecond ? "block" : "hidden"} z-10 rounded-b-2xl bg-slate-50 absolute flex flex-col shadow-xl mt-3 p-6`}>
-            <Link to={'/home/about'}><span>About Us</span></Link>
+            <Link onClick={handlerClose} to={'/home/about'}><span>About Us</span></Link>
             <div className="w-full bg-slate-300 h-0.5 my-2 "></div>
-            <Link to={'/home/policy'}><span>User Policy</span></Link>
+            <Link onClick={handlerClose} to={'/home/policy'}><span>User Policy</span></Link>
         </div>
     )
         ;
