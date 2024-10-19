1f7fee38aa488f4e251fa20844f3eac2b294fa45
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2024-10-19 04:59:08 +0300

    backend...

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -1,19 +1,182 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./UserProfile.css";
 import Event from "../Dashboard/Event";
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
+
+function getToken() {
+  return Cookies.get('token');
+}
+
+async function getUserName() {
+  try {
+      const token = getToken();
+      const response = await fetch('/api/get_user_name', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              token: token
+          }),
+      });
+
+      const data = await response.json();
+      return String(data.userName);
+      
+  } catch (error) {
+      console.log('Ошибка при получении имени пользователя:', error);
+  }
+};
+
+async function getUserMail() {
+  try {
+      const token = getToken();
+      const response = await fetch('/api/get_user_mail', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              token: token
+          }),
+      });
+
+      const data = await response.json();
+      return String(data.userMail);
+      
+  } catch (error) {
+      console.log('Ошибка при получении имени пользователя:', error);
+  }
+};
+
+async function getUserNickName() {
+  try {
+      const token = getToken();
+      const response = await fetch('/api/get_user_nickname', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              token: token
+          }),
+      });
+      
+      const data = await response.json();
+      return String(data.userNickName);
+  } catch (error) {
+      console.log('Ошибка при получении никнейма пользователя:', error);
+  }
+};
+
+async function getUserTechStack() {
+  try {
+      const token = getToken();
+      const response = await fetch('/api/get_user_techstack', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              token: token
+          }),
+      });
+      
+      const data = await response.json();
+      console.log('keek: ', data);
+
+      return String(data.userTechStack);
+  } catch (error) {
+      console.log('Ошибка при получении никнейма пользователя:', error);
+  }
+};
+
+async function listUserDashboard(__id) {
+  try {
+      const response = await fetch('/dashboard_api/load_dashboard', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            _id: __id
+          }),
+      });
+
+      const data = await response.json();
+      return data.dashboardContent;
+  } catch (error) {
+      console.log('Ошибка при получении имени пользователя:', error);
+  }
+};
+
+async function loadUserID() {
+  try {
+      const token = getToken();
+      const response = await fetch('/messenger_api/get_user_id', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              token: token
+          }),
+      });
+
+      const data = await response.json();
+      return data.userID;
+  } catch (error) {
+      console.log('Ошибка при получении имени пользователя:', error);
+  }
+};
+
+async function setUserData(_nickname, _email, _techStack) {
+  try {
+      const token = getToken();
+      const response = await fetch('/api/set_user_data', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            token: token,
+            nickname: _nickname,
+            email: _email,
+            techStack: _techStack
+          }),
+      });
+
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.log('Ошибка при получении имени пользователя:', error);
+  }
+};
 
 function Modal({ isOpen, onClose, onSubmit }) {
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
   const [avatar, setAvatar] = useState(null);
   const [techStack, setTechStack] = useState("");
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    async function invoke() {
+      setNickname(await getUserNickName());
+      setEmail(await getUserMail());
+      setTechStack(await getUserTechStack());
+    }
+    invoke();
+  }, [navigate]);
   if (!isOpen) return null;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit({ nickname, email, avatar, techStack });
+    setUserData(nickname, email, techStack);
     onClose();
+    navigate(0); // Перезагружает текущую страницу
   };
 
   return (
@@ -62,7 +225,9 @@ function Modal({ isOpen, onClose, onSubmit }) {
   );
 }
 
+// ============================================
 export default function UserProfile() {
+  const [dashboardContent, setDashboardContent] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
   const [userData, setUserData] = useState({
     name: "Aleksandr",
@@ -70,6 +235,26 @@ export default function UserProfile() {
     email: "example@mail.com",
     techStack: "Full-Stack",
   });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function invoke() {
+      const userID = await loadUserID();
+      const _dashboardContent = await listUserDashboard(userID);
+
+      // загружаем инфу о пользователе:
+      const _userData = {
+        name: await getUserName(),
+        nickname: await getUserNickName(),
+        email: await getUserMail(),
+        techStack: await getUserTechStack()
+      };
+      setUserData(_userData);
+
+      setDashboardContent(_dashboardContent);
+    }
+    invoke();
+  }, [navigate]);
 
   const handleEditClick = () => {
     setModalOpen(true);
@@ -142,36 +327,18 @@ export default function UserProfile() {
       </div>
       <div className="profile_events">
         <div className="events__profile">
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>
-        <Event 
-        image={"/icons/defaultProfile.png"}
-        event_title={"Added new tasks"}
-        what_todo={"Develop a visual concept"}
-        time={"15:47"}/>        
+          {Object.keys(dashboardContent).map((key) => {
+            const item = dashboardContent[key];
+            return (
+              <Event 
+                key={key}
+                image={"/icons/defaultProfile.png"}
+                event_title={item.event}
+                what_todo={item.name}
+                time={item.time}
+              />
+            );
+          })}     
         </div>
       </div>
       <Modal
