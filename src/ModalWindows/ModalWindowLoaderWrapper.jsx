51675778708862b1982a2b7ac56b53c10492ad0d
crockshine <161947005+crockshine@users.noreply.github.com>
2024-10-22 16:51:39 +0300

    add static width on left bar

--- src/ModalWindows/ModalWindowLoaderWrapper.jsx
+++ src/ModalWindows/ModalWindowLoaderWrapper.jsx
@@ -2,7 +2,7 @@ import {observer} from "mobx-react-lite";
 
 const ModalWindowLoaderWrapper = observer(({ isOpen}) => {
     return (
-        <div className={`${isOpen ? "block" : "hidden"} left-0 w-screen h-screen   z-30 absolute `}>
+        <div className={`${isOpen ? "block" : "hidden"} left-0 w-screen h-screen backdrop-blur-xl  z-30 absolute `}>
             <div className={`  absolute flex  w-full h-full items-center justify-center   opacity-70 z-30`}>
                 <img src="/image/Loader.gif" alt=""/>
             </div>
